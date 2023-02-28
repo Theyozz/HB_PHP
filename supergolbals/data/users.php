35cfed4b70f7fc34a78809cee7f77d3ea87d54d4
Théo VM <theomaurin875@gmail.com>
2023-02-28 14:08:25 +0100

    ajout superglob

---
+++ supergolbals/data/users.php
@@ -0,0 +1,12 @@
+<?php 
+$users = [
+    [
+        'id' => 1,
+        'email' => 'email@test.com',
+        'name' => 'Dylan',
+        'firstname' => 'Bob',
+        'profile_pic' => 'http://unsplash.it/g/180/180?random&gravity=center'
+    ],
+];
+
+?>

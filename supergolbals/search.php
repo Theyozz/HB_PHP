b9a2f122fa89492620df4abf50d1ae53cd221076
Théo VM <theomaurin875@gmail.com>
2023-03-01 09:22:40 +0100

    Update

--- supergolbals/search.php
+++ supergolbals/search.php
@@ -1,3 +1,9 @@
 <?php 
 // Recherche utilisateur par son nom
 // https://localhost2334/search?q=mick
+
+// Récupération des données
+require_once 'data/users.php';
+
+// Récupération du paramètre d'URL
+['q'=> $search] = $_GET; 
